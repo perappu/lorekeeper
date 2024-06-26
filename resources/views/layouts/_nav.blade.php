95ccbeed4df32d725955fdc527aa0be4ca84b63d
Stokori <stokoriel@gmail.com>
2024-06-25 19:01:55 -0500

    add to nav

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -73,6 +73,9 @@
                             <a class="dropdown-item" href="{{ url('trades/open') }}">
                                 Trades
                             </a>
+                            <a class="dropdown-item" href="{{ url('generators') }}">
+                                Random Generators
+                            </a>
                         </div>
                     </li>
                 @endif
