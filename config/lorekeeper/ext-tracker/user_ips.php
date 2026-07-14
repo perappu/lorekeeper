343f0b861605ae3e2f9bfedf36a448193538ad6b
Kazimir <stokoriel@gmail.com>
2026-07-14 14:32:07 -0500

    feat: add credits

---
+++ config/lorekeeper/ext-tracker/user_ips.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'wiki_key' => 'User_IPs',
+    'creators' => json_encode([
+        'ScuffedNewt' => 'https://github.com/ScuffedNewt/',
+    ]),
+    'version'  => '1.0.0',
+];
