963c1810684df955ce9ac9e095776091be05e955
Kazimir <stokoriel@gmail.com>
2026-07-14 14:30:10 -0500

    feat: update credits

---
+++ config/lorekeeper/ext-tracker/character_relationships.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'wiki_key' => 'Character_Relationships',
+    'creators' => json_encode([
+        'ScuffedNewt' => 'https://github.com/ScuffedNewt/',
+    ]),
+    'version'  => '1.0.0',
+];
