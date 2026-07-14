276b2ac9542f7133722116c4481d936578ad02ba
perappu <perappu@users.noreply.github.com>
2026-07-14 19:16:12 +0000

    refactor: fix PHP styling

--- config/lorekeeper/ext-tracker/mod_mail.php
+++ config/lorekeeper/ext-tracker/mod_mail.php
@@ -4,7 +4,7 @@ return [
     'wiki_key' => 'Mod_Mail',
     'creators' => json_encode([
         'AnimatedCritter' => 'https://github.com/AnimatedCritter',
-        'ScuffedNewt' => 'https://github.com/ScuffedNewt/',
+        'ScuffedNewt'     => 'https://github.com/ScuffedNewt/',
     ]),
     'version'  => '3.0.0',
 ];
