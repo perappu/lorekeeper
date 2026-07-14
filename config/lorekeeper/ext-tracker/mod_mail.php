6fbf9303deabc2c9ebc626376c93b21150025b9f
Kazimir <stokoriel@gmail.com>
2026-07-14 14:12:32 -0500

    chore: add extension tracker credits

---
+++ config/lorekeeper/ext-tracker/mod_mail.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'wiki_key' => 'Mod_Mail',
+    'creators' => json_encode([
+        'AnimatedCritter' => 'https://github.com/AnimatedCritter',
+        'ScuffedNewt' => 'https://github.com/ScuffedNewt/',
+    ]),
+    'version'  => '3.0.0',
+];
