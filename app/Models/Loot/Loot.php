a039ba68ee1ea0984e577bade5169a54b3e2e633
Kazimir <stokoriel@gmail.com>
2026-09-18 16:47:05 -0500

    fix: avoid nonexistent relationship by changing "None" loot rewardable_type to null

--- app/Models/Loot/Loot.php
+++ app/Models/Loot/Loot.php
@@ -3,6 +3,7 @@
 namespace App\Models\Loot;
 
 use App\Models\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Loot extends Model {
     /**
@@ -37,8 +38,8 @@ class Loot extends Model {
      * @var array
      */
     public static $createRules = [
-        'rewardable_type' => 'required',
-        'rewardable_id'   => 'required',
+        'rewardable_type' => 'nullable',
+        'rewardable_id'   => 'nullable',
         'quantity'        => 'required|integer|min:1',
         'weight'          => 'required|integer|min:1',
     ];
@@ -49,8 +50,8 @@ class Loot extends Model {
      * @var array
      */
     public static $updateRules = [
-        'rewardable_type' => 'required',
-        'rewardable_id'   => 'required',
+        'rewardable_type' => 'nullable',
+        'rewardable_id'   => 'nullable',
         'quantity'        => 'required|integer|min:1',
         'weight'          => 'required|integer|min:1',
     ];
@@ -67,5 +68,23 @@ class Loot extends Model {
     public function reward() {
         return $this->morphTo('reward', 'rewardable_type', 'rewardable_id');
         // 'None' is implicitly handled by morphTo.
+    }
+
+    /**********************************************************************************************
+
+        ACCESSORS
+
+    **********************************************************************************************/
+
+    /**
+     * Override the rewardable type value 
+     * we get None for site functions, but null for database relationship
+     */
+    protected function rewardableType(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string|null $value) => $value ? $value : 'None',
+            set: fn (string|null $value) => $value,
+        );
     }
 }
---
+++ database/migrations/2026_09_18_151526_change_none_to_null_loots.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Models\Loot\Loot;
+use App\Models\Reward\Reward;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('loots', function (Blueprint $table) {
+            if(!Schema::hasColumn('loots', 'id')) {
+                $table->id()->first();
+            }
+            $table->string('rewardable_type')->nullable()->change();
+            $table->string('rewardable_id')->nullable()->change();
+        });
+        $loots = Loot::where('rewardable_type', 'None')->get();
+        foreach($loots as $loot) {
+            $loot->update([
+                'rewardable_type' => null,
+                'rewardable_id' => null
+            ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        $loots = Loot::where('rewardable_type', null)->get();
+        foreach($loots as $loot) {
+            $loot->update([
+                'rewardable_type' => 'None',
+                'rewardable_id' => 1
+            ]);
+        }
+        Schema::table('loots', function (Blueprint $table) {
+            $table->string('rewardable_type')->change();
+            $table->integer('rewardable_id')->unsigned()->change();
+        });
+    }
+};
